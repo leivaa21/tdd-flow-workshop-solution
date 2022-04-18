6db303ee2f08f8ac4e5d90bd962c4c3bad367659
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2022-04-18 09:10:47 +0200

    First failing test

--- KataPractice1/src/FizzBuzz.ts
+++ KataPractice1/src/FizzBuzz.ts
@@ -1,1 +1,7 @@
-export class FizzBuzz {}
+export class FizzBuzz {
+  constructor(printer: () => void) {}
+
+  run(number: number) {
+    throw new Error('Method not implemented.');
+  }
+}
--- KataPractice1/test/FizzBuzz.spec.ts
+++ KataPractice1/test/FizzBuzz.spec.ts
@@ -1,3 +1,12 @@
+import { FizzBuzz } from '../src/FizzBuzz';
+
 describe('FizzBuzz', () => {
-  it('should ', () => {})
-})
+  it('should print the passed number', () => {
+    const printer = jest.fn()
+    const fizzBuzz = new FizzBuzz(printer);
+
+    fizzBuzz.run(2);
+
+    expect(printer).toHaveBeenNthCalledWith(1, '2');
+  });
+});
