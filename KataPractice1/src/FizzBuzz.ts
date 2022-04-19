82905c587a7fa9043d38ceb96192acf8d1d34885
leivaa21 <adrianleiva212@gmail.com>
2022-04-19 09:19:35 +0200

    feat: First Kata Solution

--- KataPractice1/src/FizzBuzz.ts
+++ KataPractice1/src/FizzBuzz.ts
@@ -1,11 +1,16 @@
 export class FizzBuzz {
   constructor(private printer: (line: string) => void) {}
-
   run(number: number) {
-    if (number === 3) {
-      this.printer('fizz');
-    } else {
-      this.printer(number.toString());
-    }
+    const line: string = this.getLine(number);
+    this.printer(line)
+  }
+  private getLine(number: number) {
+    const isMultipleOfThree = number % 3 === 0;
+    const isMultipleOfFive = number % 5 === 0;
+    if (isMultipleOfThree && isMultipleOfFive) return 'FizzBuzz'
+    if (isMultipleOfThree) return 'Fizz';
+    if (isMultipleOfFive) return 'Buzz';
+
+    return number.toString();
   }
 }
--- KataPractice1/test/FizzBuzz.spec.ts
+++ KataPractice1/test/FizzBuzz.spec.ts
@@ -1,23 +1,34 @@
 import { FizzBuzz } from '../src/FizzBuzz';
 
 describe('FizzBuzz', () => {
-  it('should print the passed number', () => {
-    const printer = jest.fn();
-    const fizzBuzz = new FizzBuzz(printer);
 
-    fizzBuzz.run(2);
+  let printer: jest.Mock;
+  let fizzBuzz: FizzBuzz;
 
-    expect(printer).toHaveBeenCalledTimes(1);
-    expect(printer).toHaveBeenCalledWith('2');
-  });
+  beforeEach(() => {
+    printer = jest.fn();
+    fizzBuzz = new FizzBuzz(printer); 
+  })
 
-  it('should print fizz for 3', () => {
-    const printer = jest.fn();
-    const fizzBuzz = new FizzBuzz(printer);
+  afterEach(() => {
+    expect(printer).toHaveBeenCalledTimes(1);    
+  })
 
-    fizzBuzz.run(3);
+  it.each([2,4,7])('should print the given number %p', (number) => {    
+    fizzBuzz.run(number);
+    expect(printer).toHaveBeenCalledWith(number.toString());
+  })
+  it.each([3,6,9,12])('should print fizz with %p', (number) => {
+    fizzBuzz.run(number);
+    expect(printer).toHaveBeenCalledWith('Fizz');
+  })
+  it.each([5,10,20])('should print fizz with %p', (number) => {
+    fizzBuzz.run(number);
+    expect(printer).toHaveBeenCalledWith('Buzz');
+  })
+  it.each([15,30,45])('should print fizz with %p', (number) => {
+    fizzBuzz.run(number);
+    expect(printer).toHaveBeenCalledWith('FizzBuzz');
+  })
 
-    expect(printer).toHaveBeenCalledTimes(1);
-    expect(printer).toHaveBeenCalledWith('fizz');
-  });
 });
